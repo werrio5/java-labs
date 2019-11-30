b477f0d4c1741ec4bbc63ed91947ebbb95e895fe
werrio5 <55055904+werrio5@users.noreply.github.com>
2019-11-30 14:08:13 +0300

    Add files via upload

---
+++ 0 JAR JAVAC/Jar/main/main.java
@@ -0,0 +1,9 @@
+package main;
+import notMain.*;
+
+public class main{
+public static void main(String[] args){
+System.out.println("afsdvs");
+notmain.notmain();
+}
+}
---
+++ 0 JAR JAVAC/noJar/notMain/notmain.java
@@ -0,0 +1,7 @@
+package notMain;
+
+public class notmain{
+public static void notmain(){
+System.out.println("from notmain");
+}
+}
